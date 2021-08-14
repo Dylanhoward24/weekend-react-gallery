36321e2e25f108082ed254f14b97d5e2030011f8
Dylan Howard <dylanhoward0424@outlook.com>
2021-08-14 00:26:48 -0500

    made a lot of changes

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,47 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
+// import Component into here to be used
+import GalleryList from '../GalleryList/GalleryList';
+
 function App() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Gallery of My Life</h1>
-        </header>
-        <p>Gallery goes here</p>
-        <img src="images/goat_small.jpg"/>
-      </div>
-    );
+
+  // start with an empty array
+  let [galleryData, setGalleryData] = useState([]);
+
+  // Runs when the component is first put on the DOM
+  useEffect(() => {
+    getPictures();
+  }, []);
+
+  // does a SQL query GET to retrieve pictures from data.js
+  const getPictures = () => {
+    axios
+      .get('/gallery')
+      .then((response) => {
+        console.log(response);
+        // response is the array of pictures
+        setGalleryData(response.data);
+      }).catch((error) => {
+        alert(`Couldn't get pictures. Try again later.`);
+        console.log(`Error getting inventory`, error);
+      });
+  }
+
+  console.log(galleryData);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="App-title">Gallery of My Life</h1>
+      </header>
+      <GalleryList 
+        galleryData={galleryData}
+      />
+    </div>
+  );
 }
 
 export default App;
